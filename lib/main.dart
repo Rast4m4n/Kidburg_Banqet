d9928f024f1bd18135c5ab04440b40d7ec0f830d
Kirill <motovilink@yandex.ru>
2024-03-03 19:35:07 +0300

    added navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:kidburg_banquet/presentation/screens/main_banquet/main_banquet_screen.dart';
+import 'package:kidburg_banquet/presentation/navigation/app_route.dart';
 import 'package:kidburg_banquet/presentation/theme/app_theme.dart';
 
 void main() {
   final AppTheme appTheme = AppTheme();
+  final AppNavigation appNavigator = AppNavigation();
 
   runApp(MainApp(
     appTheme: appTheme,
+    appNavigation: appNavigator,
   ));
 }
 
@@ -14,17 +16,20 @@ class MainApp extends StatelessWidget {
   const MainApp({
     super.key,
     required this.appTheme,
+    required this.appNavigation,
   });
 
   final AppTheme appTheme;
-
+  final AppNavigation appNavigation;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: appTheme.appThemeLight,
-        home: MainBanquetScreen(),
+        routes: appNavigation.routes(context),
+        initialRoute: appNavigation.initialRoute,
+        // home: MainBanquetScreen(),
       ),
     );
   }
---
+++ lib/presentation/navigation/app_route.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:kidburg_banquet/presentation/screens/main_banquet/main_banquet_screen.dart';
+import 'package:kidburg_banquet/presentation/screens/preorder_form/pre_order_form_screen.dart';
+
+abstract final class AppRoute {
+  static const String mainPage = 'main';
+  static const String preOrderFormPage = 'pre-order-form-page';
+}
+
+class AppNavigation {
+  String get initialRoute => AppRoute.mainPage;
+
+  Map<String, WidgetBuilder> routes(context) => {
+        initialRoute: (context) => MainBanquetScreen(),
+        AppRoute.preOrderFormPage: (context) => const PreOrderFormScreen(),
+      };
+}
