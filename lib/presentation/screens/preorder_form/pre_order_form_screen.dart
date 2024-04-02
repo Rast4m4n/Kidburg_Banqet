a2f0513e16542f2ba5b91aaa517ca69e4e6b4595
Kirill <motovilink@yandex.ru>
2024-04-03 01:21:11 +0300

    add mock product position in preorder page

--- lib/presentation/screens/preorder_form/pre_order_form_screen.dart
+++ lib/presentation/screens/preorder_form/pre_order_form_screen.dart
@@ -3,6 +3,7 @@ import 'package:kidburg_banquet/data/repository/excel_repository.dart';
 import 'package:kidburg_banquet/presentation/screens/preorder_form/pre_order_form_vm.dart';
 import 'package:kidburg_banquet/presentation/theme/app_paddings.dart';
 import 'package:kidburg_banquet/presentation/theme/app_theme.dart';
+import 'package:kidburg_banquet/presentation/widgets/custom_text_field.dart';
 
 class PreOrderFormScreen extends StatelessWidget {
   PreOrderFormScreen({super.key});
@@ -28,8 +29,64 @@ class PreOrderFormScreen extends StatelessWidget {
         child: ListView(
           children: const [
             _TitleTableForAdult(),
+            SizedBox(height: AppPadding.low),
+            RowProduct(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class RowProduct extends StatelessWidget {
+  const RowProduct({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Expanded(
+          flex: 2,
+          child: _BoxInformationProduct(
+            name: 'Салат оливье',
+          ),
+        ),
+        const SizedBox(width: AppPadding.low),
+        Expanded(
+          flex: 1,
+          child: CustomTextField(
+            controller: TextEditingController(),
+            label: 'Кол-во',
+          ),
+        ),
+        const SizedBox(width: AppPadding.low),
+        const Expanded(
+          flex: 2,
+          child: _BoxInformationProduct(name: 'Сумма'),
+        ),
+      ],
+    );
+  }
+}
+
+class _BoxInformationProduct extends StatelessWidget {
+  const _BoxInformationProduct({
+    super.key,
+    required this.name,
+  });
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: AppColor.textFieldColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(AppPadding.low),
+        child: Text(name),
       ),
     );
   }
