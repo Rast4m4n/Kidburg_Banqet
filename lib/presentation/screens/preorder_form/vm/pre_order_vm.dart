0b8bf16c3ee0a2eef980e92d09a85b1e5a477cbf
Kirill <motovilink@yandex.ru>
2024-06-23 13:19:21 +0300

    minor changes, moved animation logic

--- lib/presentation/screens/preorder_form/vm/pre_order_vm.dart
+++ lib/presentation/screens/preorder_form/vm/pre_order_vm.dart
@@ -1,14 +1,41 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:kidburg_banquet/data/repository/excel_repository.dart';
 import 'package:kidburg_banquet/domain/model/table_model.dart';
 
 class PreOrderViewModel with ChangeNotifier {
   PreOrderViewModel({
     required this.excelRepository,
+    required this.scrollController,
   });
 
   final ExcelRepository excelRepository;
+
+  final ScrollController scrollController;
+  ValueNotifier<bool> isVisible = ValueNotifier(true);
+
+  FloatingActionButtonLocation get buttonLocation => isVisible.value
+      ? FloatingActionButtonLocation.miniEndDocked
+      : FloatingActionButtonLocation.endFloat;
+
+  void listenScrollController() {
+    switch (scrollController.position.userScrollDirection) {
+      case ScrollDirection.idle:
+        break;
+      case ScrollDirection.forward:
+        _showNavBar();
+      case ScrollDirection.reverse:
+        _hideNavBar();
+    }
+  }
+
+  void _showNavBar() {
+    if (!isVisible.value) isVisible.value = true;
+  }
+
+  void _hideNavBar() {
+    if (isVisible.value) isVisible.value = false;
+  }
 
   Future<List<TableModel>> readDataFromExcel() async {
     return await excelRepository.readDataExcel();
