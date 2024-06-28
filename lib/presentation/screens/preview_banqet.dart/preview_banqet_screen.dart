17e6699af39925aab4b8829d706732135ba53d4e
Kirill <motovilink@yandex.ru>
2024-06-28 19:31:30 +0300

    create preview screen with preorder for dishes

---
+++ lib/presentation/screens/preview_banqet.dart/preview_banqet_screen.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+import 'package:kidburg_banquet/domain/model/banqet_model.dart';
+import 'package:kidburg_banquet/domain/model/table_model.dart';
+import 'package:kidburg_banquet/presentation/screens/preview_banqet.dart/preview_banquet_vm.dart';
+import 'package:kidburg_banquet/presentation/theme/app_paddings.dart';
+import 'package:kidburg_banquet/presentation/theme/app_theme.dart';
+import 'package:provider/provider.dart';
+
+class PreviewBanqetScreen extends StatefulWidget {
+  const PreviewBanqetScreen({super.key});
+
+  @override
+  State<PreviewBanqetScreen> createState() => _PreviewBanqetScreenState();
+}
+
+class _PreviewBanqetScreenState extends State<PreviewBanqetScreen> {
+  late final PreviewBanquerViewModel vm;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final args = ModalRoute.of(context)!.settings.arguments as BanqetModel;
+    vm = PreviewBanquerViewModel(banqetModel: args);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => vm,
+      child: Scaffold(
+        appBar: AppBar(
+          forceMaterialTransparency: true,
+        ),
+        backgroundColor: AppColor.previewBackgroundColor,
+        body: Padding(
+          padding: const EdgeInsets.all(AppPadding.big),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              Text(
+                'Кидбург банкеты',
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              const SizedBox(height: AppPadding.extra),
+              const _EventCardWidget(),
+              const SizedBox(height: AppPadding.extra),
+              const _ListCardsServingWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _EventCardWidget extends StatelessWidget {
+  const _EventCardWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PreviewBanquerViewModel>(
+      builder: (context, vm, child) {
+        return DecoratedBox(
+          decoration: const BoxDecoration(
+            color: AppColor.cardPreviewColor,
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: AppPadding.extra,
+              vertical: 32,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'МЕРОПРИЯТИЕ',
+                      style:
+                          Theme.of(context).textTheme.headlineLarge?.copyWith(
+                                color: AppColor.titleCardPreviewColor,
+                                fontSize: 24,
+                              ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: AppPadding.low),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Имя: ${vm.banqetModel.nameClient}',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: AppColor.titleCardPreviewColor,
+                            fontSize: 16,
+                          ),
+                    ),
+                    Text(
+                      'Дата: ${vm.banqetModel.dateStart.day}.${vm.banqetModel.dateStart.month}.${vm.banqetModel.dateStart.year}',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: AppColor.titleCardPreviewColor,
+                            fontSize: 16,
+                          ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: AppPadding.low),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Домик: ${vm.banqetModel.place}',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: AppColor.infoCardPreviewColor,
+                            fontSize: 14,
+                          ),
+                    ),
+                    Text(
+                      'Время: ${vm.banqetModel.firstTimeServing.format(context)}',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: AppColor.infoCardPreviewColor,
+                            fontSize: 14,
+                          ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: AppPadding.low),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Детей: ${vm.banqetModel.amountOfChildren}',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: AppColor.infoCardPreviewColor,
+                            fontSize: 14,
+                          ),
+                    ),
+                    Text(
+                      'Взрослых: ${vm.banqetModel.amountOfAdult}',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: AppColor.infoCardPreviewColor,
+                            fontSize: 14,
+                          ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _ListCardsServingWidget extends StatelessWidget {
+  const _ListCardsServingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PreviewBanquerViewModel>(
+      builder: (context, vm, child) {
+        final tables = vm.banqetModel.tables;
+        return Column(
+          children: tables!.map(
+            (table) {
+              return Column(
+                children: [
+                  _ServingDishesCardWidget(table: table),
+                  const SizedBox(height: AppPadding.extra),
+                ],
+              );
+            },
+          ).toList(),
+        );
+      },
+    );
+  }
+}
+
+class _ServingDishesCardWidget extends StatelessWidget {
+  const _ServingDishesCardWidget({
+    super.key,
+    required this.table,
+  });
+  final TableModel table;
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PreviewBanquerViewModel>(
+      builder: (context, vm, child) {
+        final dishes = vm.spreadList(table);
+        return DecoratedBox(
+          decoration: const BoxDecoration(
+            color: AppColor.cardPreviewColor,
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: AppPadding.extra,
+                  vertical: 32,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          table.name,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineLarge
+                              ?.copyWith(
+                                color: AppColor.titleCardPreviewColor,
+                                fontSize: 24,
+                              ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: AppPadding.low),
+                    ...dishes.map(
+                      (dish) {
+                        return Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ConstrainedBox(
+                                  constraints:
+                                      const BoxConstraints(maxWidth: 150),
+                                  child: Text(
+                                    dish.nameDish!,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyLarge
+                                        ?.copyWith(
+                                          color: AppColor.infoCardPreviewColor,
+                                          fontSize: 14,
+                                        ),
+                                  ),
+                                ),
+                                Text(
+                                  '${dish.count}шт',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge
+                                      ?.copyWith(
+                                        color: AppColor.infoCardPreviewColor,
+                                        fontSize: 14,
+                                      ),
+                                ),
+                              ],
+                            ),
+                            const Divider(),
+                          ],
+                        );
+                      },
+                    ).toList(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/presentation/screens/preview_banqet.dart/preview_banquet_vm.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:kidburg_banquet/domain/model/banqet_model.dart';
+import 'package:kidburg_banquet/domain/model/dish_model.dart';
+import 'package:kidburg_banquet/domain/model/table_model.dart';
+
+class PreviewBanquerViewModel with ChangeNotifier {
+  PreviewBanquerViewModel({
+    required this.banqetModel,
+  });
+  final BanqetModel banqetModel;
+  bool isExpandedCard = false;
+
+  List<DishModel> spreadList(TableModel table) {
+    List<DishModel> dishes = [];
+    for (var category in table.categories) {
+      dishes.addAll(category.dishes);
+    }
+    dishes.removeWhere((dish) => dish.count == null);
+    return dishes;
+  }
+}
