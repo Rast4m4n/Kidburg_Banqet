9d817062f110995481e20c3992576d274f5d0dbd
Kirill <motovilink@yandex.ru>
2024-06-28 19:30:19 +0300

    new route preview screen

--- lib/presentation/navigation/app_route.dart
+++ lib/presentation/navigation/app_route.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:kidburg_banquet/presentation/screens/main_banquet/main_banquet_screen.dart';
 import 'package:kidburg_banquet/presentation/screens/preorder_form/pre_order_form_screen.dart';
+import 'package:kidburg_banquet/presentation/screens/preview_banqet.dart/preview_banqet_screen.dart';
 
 abstract final class AppRoute {
   static const String mainPage = 'main';
   static const String preOrderFormPage = 'pre-order-form-page';
+  static const String previewBanquetPage = 'preview-banquet-page';
 }
 
 class AppNavigation {
@@ -13,5 +15,6 @@ class AppNavigation {
   Map<String, WidgetBuilder> routes(context) => {
         initialRoute: (context) => const MainBanquetScreen(),
         AppRoute.preOrderFormPage: (context) => const PreOrderFormScreen(),
+        AppRoute.previewBanquetPage: (context) => const PreviewBanqetScreen(),
       };
 }
