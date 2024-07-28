5e8bf54fb35ba2e32cfc027f13551fec0160e6bf
Kirill <motovilink@yandex.ru>
2024-07-28 15:49:45 +0300

    started rewriting the receipt of these dishes and the pre-order page

---
+++ lib/domain/model/dish_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dish_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DishModel _$DishModelFromJson(Map<String, dynamic> json) => DishModel(
+      // rowIndex: (json['rowIndex'] as num).toInt(),
+      nameDish: json['nameDish'] as String?,
+      weight: json['weight'] as String?,
+      price: json['price'] as int?,
+      count: (json['count'] as num?)?.toInt(),
+    );
+
+Map<String, dynamic> _$DishModelToJson(DishModel instance) => <String, dynamic>{
+      // 'rowIndex': instance.rowIndex,
+      'nameDish': instance.nameDish,
+      'weight': instance.weight,
+      'count': instance.count,
+      'price': instance.price,
+    };
