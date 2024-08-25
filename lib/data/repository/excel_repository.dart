b3fb5848e723c50f2a5995352d2bb1116c1bd4e5
Kirill <motovilink@yandex.ru>
2024-08-25 13:31:56 +0300

    change path directory for save file excel

--- lib/data/repository/excel_repository.dart
+++ lib/data/repository/excel_repository.dart
@@ -11,7 +11,7 @@ class ExcelRepository {
     Sheet sheet = excel.tables.values.first;
 
     final nameFile = FileManager.getFileName(banquet);
-    String filePath = FileManager.filePath(nameFile);
+    String filePath = FileManager.filePath(nameFile, banquet);
     await FileManager.existsDirectory();
 
     final ExcelBuilderController excelBuilder =
