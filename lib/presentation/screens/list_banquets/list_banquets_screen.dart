5d34fe9af547463ce5c57a9b5fb5fc9f3441154f
Kirill <motovilink@yandex.ru>
2024-09-02 22:07:35 +0300

    view list excel files from directories

--- lib/presentation/screens/list_banquets/list_banquets_screen.dart
+++ lib/presentation/screens/list_banquets/list_banquets_screen.dart
@@ -1,11 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:kidburg_banquet/presentation/widgets/custom_drawer.dart';
+import 'dart:io';
 
-class ListBanquetsScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:kidburg_banquet/presentation/screens/list_banquets/list_banquets_vm.dart';
+import 'package:kidburg_banquet/presentation/widgets/custom_drawer.dart';
+import 'package:provider/provider.dart';
+
+class ListBanquetsScreen extends StatefulWidget {
   const ListBanquetsScreen({super.key});
 
   @override
+  State<ListBanquetsScreen> createState() => _ListBanquetsScreenState();
+}
+
+class _ListBanquetsScreenState extends State<ListBanquetsScreen> {
+  final vm = ListBanquetVM();
+
+  @override
   Widget build(BuildContext context) {
+    final directories = vm.sortedDirectory();
     return Scaffold(
       endDrawer: const CustomDrawer(),
       appBar: AppBar(
@@ -21,7 +33,123 @@ class ListBanquetsScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: const Column(),
+      body: Provider(
+        create: (context) => vm,
+        child: ListView.builder(
+          addAutomaticKeepAlives: false,
+          addRepaintBoundaries: false,
+          itemCount: directories.length,
+          itemBuilder: (context, index) {
+            final directoryYear = directories[index];
+            return Column(
+              children: [
+                ListTile(
+                  title:
+                      Center(child: Text(directoryYear.path.split('/').last)),
+                  titleTextStyle: Theme.of(context).textTheme.headlineMedium,
+                ),
+                _DirectoryMonthWidget(directoryYear: directoryYear),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _DirectoryMonthWidget extends StatelessWidget {
+  const _DirectoryMonthWidget({
+    super.key,
+    required this.directoryYear,
+  });
+
+  final FileSystemEntity directoryYear;
+
+  @override
+  Widget build(BuildContext context) {
+    final vm = context.read<ListBanquetVM>();
+    final directoriesMonth = vm.sortedDirectoryMonth(directoryYear);
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      addAutomaticKeepAlives: false,
+      addRepaintBoundaries: false,
+      shrinkWrap: true,
+      itemCount: directoriesMonth.length,
+      itemBuilder: (context, index) {
+        final directoryMonth = directoriesMonth[index];
+        return Column(
+          children: [
+            ListTile(
+              title: Center(child: Text(directoryMonth.path.split('/').last)),
+              titleTextStyle: Theme.of(context).textTheme.bodyLarge,
+            ),
+            _FileWidget(directoryMonth: directoryMonth),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _FileWidget extends StatefulWidget {
+  const _FileWidget({
+    super.key,
+    required this.directoryMonth,
+  });
+
+  final FileSystemEntity directoryMonth;
+
+  @override
+  State<_FileWidget> createState() => _FileWidgetState();
+}
+
+class _FileWidgetState extends State<_FileWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final vm = context.read<ListBanquetVM>();
+    final files = vm.sortedFilesByDateInName(widget.directoryMonth);
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      addAutomaticKeepAlives: false,
+      addRepaintBoundaries: false,
+      shrinkWrap: true,
+      itemCount: files.length,
+      itemBuilder: (context, index) {
+        final file = files[index];
+        return Card(
+          child: ListTile(
+            contentPadding: const EdgeInsets.all(8),
+            title: Text(file.path.split('/').last),
+            trailing: PopupMenuButton(
+              icon: const Icon(Icons.more_vert),
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(
+                    child: const Text('Редактировать'),
+                    onTap: () async {
+                      vm.editFile(file, context);
+                    },
+                  ),
+                  const PopupMenuItem(
+                    child: Text('Отправить'),
+                  ),
+                  PopupMenuItem(
+                    child: const Text('Удалить'),
+                    onTap: () async {
+                      vm.deleteFile(file);
+                      setState(() {});
+                    },
+                  ),
+                ];
+              },
+            ),
+            onTap: () async {
+              vm.openFile(file);
+            },
+          ),
+        );
+      },
     );
   }
 }
--- lib/presentation/screens/list_banquets/list_banquets_vm.dart
+++ lib/presentation/screens/list_banquets/list_banquets_vm.dart
@@ -1,1 +1,63 @@
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:kidburg_banquet/data/file_manager/file_manager.dart';
+import 'package:kidburg_banquet/data/repository/excel_repository.dart';
+import 'package:kidburg_banquet/presentation/navigation/app_route.dart';
+import 'package:open_filex/open_filex.dart';
+
+class ListBanquetVM {
+  final Directory directory = FileManager.directory;
+
+  Future<void> openFile(FileSystemEntity file) async {
+    await OpenFilex.open(file.path);
+  }
+
+  Future<void> deleteFile(FileSystemEntity file) async {
+    await File(file.path).delete();
+  }
+
+  Future<void> editFile(FileSystemEntity file, BuildContext context) async {
+    final excelRepo = ExcelRepository();
+    final banquet = await excelRepo.editExcelFile(file.path);
+    print(banquet.toString());
+    if (context.mounted) {
+      Navigator.of(context).pushNamed(AppRoute.mainPage, arguments: banquet);
+    }
+  }
+
+  List<FileSystemEntity> sortedDirectory() {
+    final directories = directory.listSync()
+      ..sort(
+          (a, b) => b.path.split('/').last.compareTo(a.path.split('/').last));
+    return directories;
+  }
+
+  List<FileSystemEntity> sortedDirectoryMonth(FileSystemEntity directoryYear) {
+    final directoriesMonth = Directory(directoryYear.path).listSync()
+      ..sort((a, b) {
+        return DateFormat("MMMM")
+            .parse(b.path.split('/').last)
+            .compareTo(DateFormat("MMMM").parse(a.path.split('/').last));
+      });
+
+    return directoriesMonth;
+  }
+
+  List<FileSystemEntity> sortedFilesByDateInName(
+      FileSystemEntity directoryMonth) {
+    final files = Directory(directoryMonth.path).listSync();
+    files.sort((a, b) {
+      final aName = a.path.split('/').last;
+      final bName = b.path.split('/').last;
+      // Извлекаем число из названия файла с помощью регулярного выражения
+      final aNumber =
+          int.tryParse(RegExp(r'\d+').firstMatch(aName)?.group(0) ?? '0') ?? 0;
+      final bNumber =
+          int.tryParse(RegExp(r'\d+').firstMatch(bName)?.group(0) ?? '0') ?? 0;
+      return aNumber.compareTo(bNumber);
+    });
+    return files;
+  }
+}
